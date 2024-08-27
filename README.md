97e3564d696a818e19490d32dcdbfba25d577e9b
msi <thatnaing336@gmail.com>
2024-08-27 12:02:16 +0630

    README file update

--- README.md
+++ README.md
@@ -64,3 +64,15 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Project Install
+
+composer install --ignore-platform-reqs 
+
+yarn install 
+
+yarn run dev
+
+php artisan server
+
+php artisan reverb:start
---
+++ resources/js/Pages/VisitCount.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const messages = ref([]);
+// const count = ref(0);
+// Echo.channel("messages").listen("HelloWorld", (e) => {
+//     console.log(e);
+//     count.value++;
+// });
+const message = ref("");
+function handleSubmit() {
+    messages.value.push({
+        id: crypto.randomUUID(),
+        // id: messages.value.length + 1,
+        message: message.value,
+    });
+    message.value = "";
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="message in messages" :key="message.id">
+            {{ message.message }}
+        </li>
+    </ul>
+    <form @submit.prevent="handleSubmit">
+        <input v-model="message"></input>
+        <button>Send Message</button>
+    </form>
+</template>
